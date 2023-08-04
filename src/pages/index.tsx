588bcaa0d6db86d494582f3eadee32bee8985ae0
tagrn <fksk94@gmail.com>
2023-08-04 00:10:27 -0700

    refactor: title, description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="꾸준히 공부하고 기록합니다.">
       <main>
         <HomepageFeatures />
       </main>
