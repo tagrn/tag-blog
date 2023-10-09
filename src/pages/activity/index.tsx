936e749ac88c67d57fac670ce5eb249e0e45e608
tagrn <fksk94@gmail.com>
2023-10-09 19:49:44 +0900

    chore: update activity

--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -21,8 +21,8 @@ export default function Home(): JSX.Element {
               <Link to={'https://chlngers.onelink.me/Ju7U/c25w3g8d'}>
                 참가 링크
               </Link>
-              <div>2022.07.18 ~ 진행 중</div>
-              <div>2023.10.01 현재 누적 참가 298명</div>
+              <div>2022.07 ~ 진행 중</div>
+              <div>2023.10.09 현재 누적 참가 303명</div>
             </div>
           }
         />
@@ -31,9 +31,9 @@ export default function Home(): JSX.Element {
           content={
             <div>
               <div>개발관련 이슈나 문제해결, 배운 점을 정리</div>
-              <Link to={'https://blog.taewan.link/'}>블로그 링크</Link>
-              <div>2023.07.01 ~ 진행 중</div>
-              <div>Docusaurus를 사용하여 블로그 개설</div>
+              <Link to={'https://blog.taewan.link/'}>현재 블로그 링크</Link>
+              <div>2019.10 ~ 진행 중</div>
+              <div>2023.07에 Docusaurus를 이용하여 새로운 블로그 개설</div>
             </div>
           }
         />
@@ -45,8 +45,8 @@ export default function Home(): JSX.Element {
               <Link to={'https://leetcode.com/Taewan-Gu'}>
                 릿코드 계정 링크
               </Link>
-              <div>2023.08.01 ~ 진행 중</div>
-              <div>2023.10.01 현재 Redeem: 1074</div>
+              <div>2023.08 ~ 진행 중</div>
+              <div>2023.10.09 현재 Redeem: 1120</div>
             </div>
           }
         />
